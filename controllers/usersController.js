2d4cad29737da5930bcb6ac3d333346fd036bf98
Timothy Allgood <timothyallgood@gmail.com>
2020-07-10 14:24:17 -0500

    added comments to make code easier to understand

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const db = require('../models');
 
+// Shows All Users
 router.get('/', (req, res) => {
 	db.User.find({}, (err, foundUsers) => {
 		if (err) console.log(err);
@@ -12,10 +13,12 @@ router.get('/', (req, res) => {
 	});
 });
 
+// Displays sign up form
 router.get('/create', (req, res) => {
 	res.render('user/create');
 });
 
+// Creates new user (sign up)
 router.post('/', (req, res) => {
 	db.User.create(req.body, (err, createdUser) => {
 		if (err) console.log(err);
@@ -23,28 +26,36 @@ router.post('/', (req, res) => {
 	});
 });
 
-// This route will set all the session info
+// Displays Sign In Form
 router.get('/signin', (req, res) => {
 	res.render('user/signin');
 });
+
+// This route will set all the session info, signs user in
 
 router.post('/signin', (req, res) => {
 	db.User.findOne({ username: req.body.username }, (err, currentUser) => {
 		if (err) console.log(err);
 		if (currentUser.username) {
+			// If user exists
 			if (currentUser.password === req.body.password) {
-				req.session.userId = currentUser._id;
-				req.session.username = currentUser.username;
-				req.session.admin = currentUser.isAdmin;
-				res.redirect(`/users/${req.session.userId}`);
+				// Checks if password matches password for user in database
+				req.session.userId = currentUser._id; // Sets session userId to loggedIn userId
+				req.session.username = currentUser.username; // Sets session username to loggedIn username
+				req.session.admin = currentUser.isAdmin; // Sets session admin status to loggedIn admin status
+				res.redirect(`/users/${req.session.userId}`); // Redirects to logged in users page
 			} else {
+				// If password doesn't match
 				res.send('Wrong password');
 			}
 		} else {
+			// If user does not exist
 			res.send('No user found');
 		}
 	});
 });
+
+// Show Individual User
 
 router.get('/:id', (req, res) => {
 	db.User.findById(req.params.id, (err, foundUser) => {

01d4f24239541a37d2972343721b16c97819fc0f
Timothy Allgood <timothyallgood@gmail.com>
2020-07-10 13:05:32 -0500

    Initial commit, added server boilerplate

---
+++ controllers/usersController.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('../models');
+
+// router.get('/', (req, res) => {
+// 	db.User.find({}, (err, foundUsers) => {
+// 		if (err) console.log(err);
+// 		res.render('user/index', {
+// 			users: foundUsers,
+// 		});
+// 	});
+// });
+
+// router.get('/create', (req, res) => {
+// 	res.render('user/create');
+// });
+
+// router.post('/', (req, res) => {
+// 	db.User.create(req.body, (err, createdUser) => {
+// 		if (err) console.log(err);
+// 		res.redirect('/');
+// 	});
+// });
+
+// // This route will set all the session info
+// router.get('/signin', (req, res) => {
+// 	res.render('user/signin');
+// });
+
+// router.post('/signin', (req, res) => {
+// 	db.User.findOne({ username: req.body.username }, (err, currentUser) => {
+// 		if (err) console.log(err);
+// 		if (currentUser.username) {
+// 			if (currentUser.password === req.body.password) {
+// 				req.session.userId = currentUser._id;
+// 				req.session.username = currentUser.username;
+// 				req.session.admin = currentUser.isAdmin;
+// 				console.log(req.session);
+// 				res.redirect(`/users/${req.session.userId}`);
+// 			} else {
+// 				res.send('Wrong password');
+// 			}
+// 		} else {
+// 			res.send('No user found');
+// 		}
+// 	});
+// });
+
+// router.get('/:id', (req, res) => {
+// 	db.User.findById(req.params.id, (err, foundUser) => {
+// 		if (err) console.log(err);
+// 		console.log(req.session);
+// 		res.render('user/show', {
+// 			user: foundUser,
+// 			seshId: req.session.userId,
+// 		});
+// 	});
+// });
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+const session = require('express-session');
+
+const usersController = require('./controllers/usersController');
+
+const PORT = process.env.PORT || 4000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(`${__dirname}/public`));
+
+// Method Override
+// app.use(methodOverride('_method'));
+
+// Express BodyParser
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(
+	session({
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized: true,
+	})
+);
+
+// Sets EJS Variables
+app.use((req, res, next) => {
+	app.locals.userId = req.session.userId;
+	app.locals.username = req.session.username;
+	app.locals.admin = req.session.admin;
+	console.log(app.locals);
+	next();
+});
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.use('/users', usersController);
+
+app.listen(PORT, () => console.log(`Server running on${PORT}`));
